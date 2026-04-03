93abc57174898b205e08edcd52e46757b217e5f9
raufokz <raufabdur886@gmail.com>
2026-04-03 12:23:00 -0400

    fix: update paths for autoload and bootstrap in index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
